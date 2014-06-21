b62a77e6a7aa1f082cfef21d5ec1f15433505e29
My Name Is Zulfindra <zulfinjuliant@gmail.com>
2014-06-21 11:36:04 +0700

    first commit bootcodeplate

---
+++ application/views/template/header.php
@@ -0,0 +1,8 @@
+<header id="header" class="navbar navbar-fixed-top">
+	<div class="container">
+		<div class="navbar">
+			<a class="navbar-brand" href="#">Title</a>
+			<?php echo $nav?>
+		</div>
+	</div>
+</header>
